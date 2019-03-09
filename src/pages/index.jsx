04a2a93e74e76fc87654259c86d9cbd09931a22d
Nathan Yocum <nayocum@nevada.unr.edu>
2019-03-09 00:04:37 -0800

    Started Gatsby rewrite to learn Gatsby

---
+++ src/pages/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FaGrin } from 'react-icons/fa';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO
+      title="Home"
+      keywords={[
+        'gatsby',
+        'react',
+        'nathanyocum',
+        'golang',
+        'javascript',
+        'testing'
+      ]}
+    />
+    <h1>Hello World!</h1>
+    <FaGrin size="300px" />
+    <p>This site is under construction!</p>
+    <p>
+      I am very busy working on my{' '}
+      <a href="https://github.com/LectureGoggles/">Senior Project</a> so I will
+      be very slow updating this site.
+    </p>
+  </Layout>
+);
+
+export default IndexPage;
